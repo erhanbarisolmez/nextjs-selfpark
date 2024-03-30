1ffdf0e2016b32512368af8df0b3f03312c2b73e
Erhan Barış Ölmez <erhanbarisolmez@gmail.com>
2024-03-30 03:26:48 +0300

    30.03.2024

--- src/components/ui/login/login-card.jsx
+++ src/components/ui/login/login-card.jsx
@@ -1,164 +1,183 @@
-'use client'
-import InputComponent from '@/components/InputComponent';
-import { useThemeHook } from '@/hooks/useThemeHook';
-import AspectRatio from '@mui/joy/AspectRatio';
-import Button from '@mui/joy/Button';
-import Card from '@mui/joy/Card';
-import CardContent from '@mui/joy/CardContent';
-import CardOverflow from '@mui/joy/CardOverflow';
-import Typography from '@mui/joy/Typography';
+"use client";
+import React, { useState } from "react";
+import InputComponent from "@/components/InputComponent";
+import { useThemeHook } from "@/hooks/useThemeHook";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Button from "@mui/joy/Button";
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import CardOverflow from "@mui/joy/CardOverflow";
+import Typography from "@mui/joy/Typography";
 import { Box, Grid } from "@mui/material";
-import Image from 'next/image';
-import Link from 'next/link';
-import { useState } from 'react';
-export default function LoginCard() {
- 
-  const { getThemeStyles } = useThemeHook();
-  const { backgroundColor, buttonColor, textColor, linkColor, lefColorBg, logoColor, whiteColor } = getThemeStyles();
+import Image from "next/image";
+import Link from "next/link";
 
-  // const [formData, setFormData] = useState({username: '', password:''});
+const LoginCard = () => {
   const [username, setUsername] = useState("");
-  const [ password, setPassword] = useState('');
-  console.log("username:" +username + "password" +password)
-  // const handleChange = (event) => {
-  //   const {name, value} = event.target;
-  //   setFormData({formData, [name]:value})
-  //   console.log(formData);
-  // }
-  
+  const [password, setPassword] = useState("");
 
- // Handle form submission
- const handleSubmit = async (event) => {
-  event.preventDefault();
-  try {
-    const response = await fetch('http://127.0.0.1:8000/auth/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      // body: JSON.stringify(formData)
-      body: JSON.stringify(username, password)
-    });
-  
+  const { getThemeStyles } = useThemeHook();
+  const {
+    backgroundColor,
+    buttonColor,
+    textColor,
+    linkColor,
+    lefColorBg,
+    logoColor,
+    whiteColor,
+  } = getThemeStyles();
 
-    if (response.ok) {
-      const data = await response.json();
-      const token = data.access_token;
-      if (token) {
-        window.location.href="dashboard";
-      }
-      console.log("Login successful", token);
-    } else {
-      console.log('Login failed:', response)
+
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!username || !password) {
+      console.error("Username and password are required");
+      return;
     }
 
-  } catch (error) {
-    console.error('Error during login', error)
-  }
-}
+    try {
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          'accept': 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+          'username': username,
+          'password': password
+        })
+      };
+      const response = await fetch(
+        "http://127.0.0.1:8000/auth/token",
+        requestOptions
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        const token = data.access_token;
+        if (token) {
+          window.location.href = "dashboard";
+        }
+        console.log("Login successful", token);
+      } else {
+        console.log("Login failed:", response);
+      }
+    } catch (error) {
+      console.error("Error during login", error);
+    }
+  };
+
   return (
-    
-      <Card
-        size="lg"
+    <Card
+      size="lg"
+      variant="solid"
+      orientation="horizontal"
+      sx={{
+        textAlign: "center",
+        maxWidth: "100%",
+        width: 800,
+        resize: "vertical",
+        overflow: "auto",
+        color: textColor,
+        backgroundColor: backgroundColor,
+      }}
+    >
+      <CardOverflow
         variant="solid"
-        orientation="horizontal"
         sx={{
-          textAlign: 'center',
-          maxWidth: '100%',
-          width: 800,
-          // to make the demo resizable
-          resize: 'vertical',
-          overflow: 'auto',
-          color: textColor,
-          backgroundColor: backgroundColor
+          flex: "0 0 200px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          px: "var(--Card-padding)",
+          backgroundColor: lefColorBg,
         }}
       >
-        <CardOverflow
-          variant="solid"
+        <AspectRatio
           sx={{
-            flex: '0 0 200px',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            px: 'var(--Card-padding)',
-            backgroundColor: lefColorBg
-
+            width: "100%",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "50%",
           }}
         >
-          <AspectRatio sx={{ width: '100%', alignItems: 'center', justifyContent: 'center', height: '50%' }}>
+          <Box>
+            <Image
+              src="/images/stlogo.png"
+              layout="responsive"
+              width={300}
+              height={300}
+              alt="logo"
+            />
+          </Box>
+        </AspectRatio>
+        <Typography
+          variant="plain"
+          level="body-xs"
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            mt: 3,
+            color: whiteColor,
+          }}
+        >
+          Discover Our Digital Intelligence Based Parking Solutions
+        </Typography>
+      </CardOverflow>
 
-            <Box>
-              <Image
-                src="/images/stlogo.png"
-                layout='responsive'
-                width={300}
-                height={300}
-                alt='logo'
-
-              />
-            </Box>
-          </AspectRatio>
-          <Typography variant='plain' level='body-xs' sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', mt: 3, color: whiteColor }}>
-            Discover Our Digital Intelligence Based Parking Solutions
-          </Typography>
-        </CardOverflow>
-
-        <CardContent sx={{ gap: 1.5, minWidth: 200, }}>
-
-         <form  onSubmit={handleSubmit} method='POST'>
-          <Grid sx={{
-            color: logoColor
-          }} >
+      <CardContent sx={{ gap: 1.5, minWidth: 200 }}>
+        <form onSubmit={handleSubmit} method="POST">
+          <Grid sx={{ color: logoColor }}>
             LOGIN
             <InputComponent
               name="username"
               value={username}
-              variant={'outlined'}
-              onChange={(e) => setUsername(e.target.value)}
+              variant={"outlined"}
+              onChange={(e)=> setUsername(e.target.value)}
             />
-
             <InputComponent
               name="password"
               value={password}
-              variant={'outlined'}
-              onChange={(e) => setPassword(e.target.value)}
+              variant={"outlined"}
+              onChange={(e)=> setPassword(e.target.value)}
             />
           </Grid>
 
           {/* LOGIN BUTTON */}
-       
-       <div>
-        
           <Button
-            type='submit'
+            type="submit"
             variant="outlined"
             sx={{
-              '--variant-borderWidth': '2px',
+              "--variant-borderWidth": "2px",
               borderRadius: 40,
-              borderColor: 'primary.500',
-              mx: 'auto',
-              color: buttonColor
+              borderColor: "primary.500",
+              mx: "auto",
+              color: buttonColor,
             }}
           >
-            
             Login
           </Button>
-          </div>
-          </form>
-          <CardContent sx={{ color: textColor }}>
-            <Typography level="title-lg" color={textColor}>Not Registered?</Typography>
-            <Typography fontSize="sm" sx={{ mt: 0.5, color: textColor }}>
-              <Link href="dashboard" style={{ textDecoration: 'none', color: linkColor }}>Register Now</Link>
-            </Typography>
+        </form>
 
-          </CardContent>
-
-
-         
+        <CardContent sx={{ color: textColor }}>
+          <Typography level="title-lg" color={textColor}>
+            Not Registered?
+          </Typography>
+          <Typography fontSize="sm" sx={{ mt: 0.5, color: textColor }}>
+            <Link
+              href="dashboard"
+              style={{ textDecoration: "none", color: linkColor }}
+            >
+              Register Now
+            </Link>
+          </Typography>
         </CardContent>
-      </Card> 
-     
+      </CardContent>
+    </Card>
   );
-}
+};
 
-
+export default LoginCard;
