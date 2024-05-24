744c10a6d229dcba2d5a627fe652da6caf72ff9e
erhanbarisolmez <61422236+erhanbarisolmez@users.noreply.github.com>
2024-05-24 16:45:50 +0300

    24.04.2024

--- src/components/ReactCamera.jsx
+++ src/components/ReactCamera.jsx
@@ -1,37 +1,61 @@
-import { useRef } from 'react';
-import Webcam from 'react-webcam';
+import { useEffect, useRef } from 'react';
 
 export const ReactCamera = () => {
   const webcamRef = useRef(null);
+  const socketRef = useRef(null);
 
-  const capture = () => {
-    const imageSrc = webcamRef.current.getScreenshot();
-    sendPhoto(imageSrc);
-  };
+  useEffect(() => {
+    // WebSocket bağlantısını oluştur
+    socketRef.current = new WebSocket('ws://192.168.4.88:8001/camera');
 
-  const sendPhoto = (photo) => {
-    const ws = new WebSocket('ws://192.168.4.88:8080');
-
-    ws.onopen = () => {
-      ws.send(JSON.stringify({ type: 'photo', payload: photo }));
+    // WebSocket bağlantısı açıldığında
+    socketRef.current.onopen = () => {
+      console.log("WebSocket connection established");
     };
+
+    // WebSocket mesajı alındığında
+    socketRef.current.onmessage = (event) => {
+      console.log("WebSocket message received", event.data);
+
+    };
+
+    // Component kaldırıldığında WebSocket bağlantısını kapat
+    return () => {
+      socketRef.current.close();
+    }
+  }, []);
+
+  const requestNotificationPermission = async () => {
+    if (Notification.permission !== 'granted') {
+      await Notification.requestPermission();
+    }
+  }
+
+  const openCamera = async () => {
+    try {
+      // Bildirim izni yoksa, kullanıcıya izin iste
+      await requestNotificationPermission();
+      // Kamera erişim izni al
+      const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
+
+      // Kamerayı video etiketine bağla
+      if (webcamRef.current) {
+        webcamRef.current.srcObject = mediaStream;
+      }
+
+      // WebSocket bağlantısı açıksa "openCamera" mesajını gönder
+      if (socketRef.current.readyState === WebSocket.OPEN) {
+        socketRef.current.send("openCamera");
+      }
+    } catch (error) {
+      console.error('Kamera erişimi sağlanamadı', error);
+    }
   };
 
   return (
     <div>
-      
-      <Webcam
-        audio={false}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-        width="100%"
-        videoConstraints={{
-          facingMode: 'user'
-        }}
-      />
-      <button onClick={capture}>Fotoğraf Çek ve Gönder</button>
+      <button id="cameraButton" onClick={openCamera}>Kamerayı Aç</button>
+      <video ref={webcamRef} autoPlay style={{ width: '100%' }}></video>
     </div>
   );
 };
-
-
