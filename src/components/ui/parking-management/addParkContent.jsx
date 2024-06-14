e3020bdf77f6b52d0565dfd48de4b132024ac71a
erhanbarisolmez <61422236+erhanbarisolmez@users.noreply.github.com>
2024-06-14 08:18:02 +0300

    addParkContent düzenlendi

--- src/components/ui/parking-management/addParkContent.jsx
+++ src/components/ui/parking-management/addParkContent.jsx
@@ -15,12 +15,13 @@ const AddParkContent = ({ searchLngLat }) => {
   const [formData, setFormData] = useState({});
   const { lat, lng } = searchLngLat || {};
   const { getModalStyles } = useThemeHook();
-  const { modalDialogBackground, textColor } = getModalStyles();
+  const { modalDialogBackground, textColor} = getModalStyles();
   const [enable, setEnable] = useState(false);
+  const [parkType, setParkType] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const serviceManager = new ServiceManager();
   const { token } = useAuth();
-
+  
   const handleToggleChange = (setter) => (event, newValue) => {
     setter(newValue);
   }
@@ -40,9 +41,9 @@ const AddParkContent = ({ searchLngLat }) => {
       lat,
       lng,
       isOpen,
-      enable
+      enable,
+      parkType : parkType ? "açık" : "kapalı"
     }
-
 
     await serviceManager.parkService.add_park(parkData, token);
 
@@ -51,9 +52,7 @@ const AddParkContent = ({ searchLngLat }) => {
   return (
     <CardComponent
       sx={{
-        backgroundColor: modalDialogBackground,
-        color: textColor
-
+        backgroundColor: modalDialogBackground
       }}
       children={
         <Grid container spacing={2}>
@@ -91,13 +90,10 @@ const AddParkContent = ({ searchLngLat }) => {
                   xs: 'column'
                 },
                 justifyContent: 'space-between',
-
-
               }}>
                 <Grid item xs={12} sm={6} mt={1}>
-                  <FormLabel>Park Name</FormLabel>
+                  <FormLabel sx={{ color:textColor}}>Park Name</FormLabel>
                   <InputComponent
-
                     placeholder={"Park Name"}
                     name='parkName'
                     value={formData.parkName}
@@ -107,7 +103,7 @@ const AddParkContent = ({ searchLngLat }) => {
                 </Grid>
 
                 <Grid item xs={12} sm={6} mt={1}>
-                  <FormLabel>City</FormLabel>
+                  <FormLabel sx={{ color:textColor}}>City</FormLabel>
                   <InputComponent
                     placeholder={"City"}
                     name='city'
@@ -117,7 +113,7 @@ const AddParkContent = ({ searchLngLat }) => {
                 </Grid>
 
                 <Grid item xs={12} sm={6} mt={1}>
-                  <FormLabel>District</FormLabel>
+                  <FormLabel sx={{ color:textColor}}>District</FormLabel>
                   <InputComponent
                     placeholder={"District"}
                     name='district'
@@ -129,7 +125,7 @@ const AddParkContent = ({ searchLngLat }) => {
 
 
                 <Grid item xs={12} sm={6} mt={1}>
-                  <FormLabel>Capacity</FormLabel>
+                  <FormLabel sx={{ color:textColor}}>Capacity</FormLabel>
                   <InputComponent
                     placeholder={"Capacity"}
                     name='capacity'
@@ -139,11 +135,8 @@ const AddParkContent = ({ searchLngLat }) => {
                   />
                 </Grid>
 
-
-
-
                 <Grid item xs={12} sm={6} mt={1}>
-                  <FormLabel>Empty Capacity</FormLabel>
+                  <FormLabel sx={{ color:textColor}}>Empty Capacity</FormLabel>
                   <InputComponent
                     placeholder={"Empty Capacity"}
                     name='emptyCapacity'
@@ -155,7 +148,7 @@ const AddParkContent = ({ searchLngLat }) => {
 
 
                 <Grid item xs={12} sm={6} mt={1}>
-                  <FormLabel>Work Hours</FormLabel>
+                  <FormLabel sx={{ color:textColor}}>Work Hours</FormLabel>
                   <InputComponent
                     placeholder={"Work Hours"}
                     name='workHours'
@@ -165,19 +158,19 @@ const AddParkContent = ({ searchLngLat }) => {
                   />
                 </Grid>
 
-                  <Grid item xs={12} sm={6} mt={1}>
-                    <FormLabel>Free Time</FormLabel>
-                    <InputComponent
-                      placeholder={"Free Time"}
-                      name='freeTime'
-                      value={formData.freeTime}
-                      onChange={handleChange}
+                <Grid item xs={12} sm={6} mt={1}>
+                  <FormLabel sx={{ color:textColor}}>Free Time</FormLabel>
+                  <InputComponent
+                    placeholder={"Free Time"}
+                    name='freeTime'
+                    value={formData.freeTime}
+                    onChange={handleChange}
 
-                    />
+                  />
 
-                  </Grid>
+                </Grid>
 
-                  <Grid item xs={12} sm={6} mt={1}>
+                {/* <Grid item xs={12} sm={6} mt={1}>
                   <FormLabel>Address</FormLabel>
                     <InputComponent
                       placeholder={"Address"}
@@ -186,90 +179,82 @@ const AddParkContent = ({ searchLngLat }) => {
                       onChange={handleChange}
 
                     />
-                  </Grid>
+                  </Grid> */}
 
 
 
-                  <Grid item xs={12} mt={2}>
-                  <FormLabel>Park Type</FormLabel>
-                    <Select
-                      variant="soft"
-                      placeholder="Park Type"
-                      value={enable}
-                      onChange={handleToggleChange(setEnable)}
-                      sx={{
-                        width: '100%'
-                      }}
-                    >
-                      <Option value={true}>
-                        Açık
-                      </Option>
-                      <Option value={false}>
-                        Kapalı
-                      </Option>
-                    </Select>
-                  </Grid>
-
-                  <Grid item xs={12} >
-                    <Select
-                      variant="soft"
-                      placeholder="DURUM"
-                      value={enable}
-                      onChange={handleToggleChange(setEnable)}
-                      sx={{
-                        width: '100%'
-                      }}
-                    >
-                      <Option value={true}>
-                        Etkin
-                      </Option>
-                      <Option value={false}>
-                        Devre Dışı
-                      </Option>
-                    </Select>
-                  </Grid>
-
-                  <Grid item xs={12}>
-                    <Select
-                      variant="soft"
-                      placeholder="DURUM"
-                      value={isOpen}
-                      onChange={handleToggleChange(setIsOpen)}
-                      sx={{
-
-                        width: '100%'
-                      }}
-                    >
-                      <Option value={true}>
-                        Etkin
-                      </Option>
-                      <Option value={false}>
-                        Devre Dışı
-                      </Option>
-
-                    </Select>
-                  </Grid>
-
-
-
-
-
-
+                <Grid item xs={12} sm={6} mt={1}>
+                  <FormLabel sx={{ color:textColor}}>Park Type</FormLabel>
+                  <Select
+                    variant="soft"
+                    placeholder="Parking  Type"
+                    value={parkType}
+                    onChange={handleToggleChange(setParkType)}
+                    sx={{
+                      width: '100%'
+                    }}
+                  >
+                    <Option value={true}>
+                      Outdoor park
+                    </Option>
+                    <Option value={false}>
+                      Indoor park
+                    </Option>
+                  </Select>
                 </Grid>
-                <Grid item xs={12} sx={{ textAlign: 'center', alignContent: 'center', alignItems: 'center', display: 'flex', justifyContent: 'center', mt: 2 }}>
-                  <ButtonComponent
-                    type="submit"
-                    text={"KAYDET"}
-                    color={"neutral"}
 
-                  />
+                <Grid item xs={12} sm={6} mt={1}>
+                <FormLabel sx={{ color:textColor}}>Enabled</FormLabel>
+                <Select
+                    variant="soft"
+                    placeholder="DURUM"
+                    value={enable}
+                    onChange={handleToggleChange(setEnable)}
+                    sx={{
+                      width: '100%'
+                    }}
+                  >
+                    <Option value={true}>
+                      True
+                    </Option>
+                    <Option value={false}>
+                      False
+                    </Option>
+                  </Select>
                 </Grid>
-           
+
+                <Grid item xs={12} sm={6} mt={1}>
+                <FormLabel sx={{ color:textColor}}>Is Open</FormLabel>
+                <Select
+                    variant="soft"
+                    placeholder="DURUM"
+                    value={isOpen}
+                    onChange={handleToggleChange(setIsOpen)}
+                    sx={{
+
+                      width: '100%'
+                    }}
+                  >
+                    <Option value={true}>
+                      Active
+                    </Option>
+                    <Option value={false}>
+                      Passive
+                    </Option>
+
+                  </Select>
+                </Grid>
 
 
+              </Grid>
+              <Grid item xs={12} sx={{ textAlign: 'center', alignContent: 'center', alignItems: 'center', display: 'flex', justifyContent: 'center', mt: 3 }}>
+                <ButtonComponent
+                  type="submit"
+                  text={"KAYDET"}
+                  color={"neutral"}
 
-
-
+                />
+              </Grid>
             </form>
           </Container>
         </Grid>
--- src/components/ui/personnel-management/listPersonnelContent.jsx
+++ src/components/ui/personnel-management/listPersonnelContent.jsx
@@ -1,70 +1,14 @@
 import { ListContent } from '@/components/ListContent';
 import { useThemeHook } from '@/hooks/useThemeHook';
 import { useState } from 'react';
-const personnelList = [
-  {
-    id: 1,
-    name: 'Ahmet',
-    capacity: 100,
-    workHours: '08:30 - 20:30',
-    freeParkingDuration: '2 hourse',
-    email: 'oıqjweı@aisjd.com',
-    role: 'Manager',
-  },
-  {
-    id: 2,
-    name: 'Ayşe',
-    capacity: 200,
-    workHours: '07:00 - 20:00',
-    freeParkingDuration: '2 hourse',
-    role: 'Employee'
-  },
-  {
-    id: 3,
-    name: 'Arzu',
-    capacity: 150,
-    workHours: '24 hours',
-    freeParkingDuration: '2 hourse',
-    role: 'Manager'
-  },
-  {
-    id: 4,
-    name: 'Murat',
-    capacity: 150,
-    workHours: '24 hours',
-    freeParkingDuration: '2 hourse',
-    role: 'Employee'
-  },
-  {
-    id: 5,
-    name: 'Mehmet',
-    capacity: 150,
-    workHours: '24 hours',
-    freeParkingDuration: '2 hourse',
-    role: 'Manager'
-  },
-  {
-    id: 6,
-    name: 'Burak',
-    capacity: 150,
-    workHours: '24 hours',
-    freeParkingDuration: '2 hourse',
-    role: 'Employee'
-  },
-  {
-    id: 7,
-    name: 'Barış',
-    capacity: 150,
-    workHours: '24 hours',
-    freeParkingDuration: '2 hourse',
-    role: 'Employee'
-  }
-]
+
 
 export const ListPersonnelContent = () => {
   const [search, setSearch] = useState("")
   const { getThemeStyles } = useThemeHook();
   const { textColor, isDarkMode, buttonColor, backgroundColor } = getThemeStyles();
+ 
+ 
   const filterList = personnelList.filter(person =>
     person.name.toLowerCase().includes(search) ||
     person.role.toLowerCase().includes(search)
@@ -78,7 +22,8 @@ export const ListPersonnelContent = () => {
       row2={'role'}
       column4={"Actions"}
       filterList={filterList}
-      setSearch={setSearch} />
+      setSearch={setSearch} 
+      />
 
   )
 }
