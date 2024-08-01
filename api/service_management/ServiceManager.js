60df8021d886e16350b3bc8f4f10635d405108f3
erhanbarisolmez <61422236+erhanbarisolmez@users.noreply.github.com>
2024-08-01 16:47:32 +0300

    personnel service güncellenecek

--- api/service_management/ServiceManager.js
+++ api/service_management/ServiceManager.js
@@ -1,6 +1,5 @@
 import AuthenticateService from "../services/AuthenticateService";
 import ParkService from "../services/ParkService";
-import PersonnelService from "../services/PersonnelService";
 
 
 export default class ServiceManager{
@@ -8,7 +7,7 @@ export default class ServiceManager{
   constructor(){
     this.authService = new AuthenticateService();
     this.parkService = new ParkService();
-    this.personnelService = new PersonnelService();
+    // this.personnelService = new PersonnelService();
   }
  
  
